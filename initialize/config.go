bb3a49d48f2cb0577e71367df2d17271572311af
sawyerwu <664269713@qq.com>
2021-04-27 22:36:06 +0800

    mysql init

---
+++ initialize/config.go
@@ -0,0 +1,33 @@
+package initialize
+
+import (
+	"fmt"
+	"github.com/sawyerwu/swu-ops/pkg/common"
+	"github.com/spf13/viper"
+	"os"
+)
+
+const (
+	configType = "yml"
+	configName = "application.yml"
+	configPath = "/conf"
+)
+
+func InitConfig() {
+	v := viper.New()
+	workDir, _ := os.Getwd()
+	v.SetConfigName(configName)
+	v.SetConfigType(configType)
+	v.AddConfigPath(workDir + configPath)
+	err := v.ReadInConfig()
+	if err != nil {
+		panic(fmt.Sprintf("Init config file failed: %v", err))
+	}
+
+	// convert config file to struct
+	if err := v.Unmarshal(&common.Configuration{}); err != nil {
+		panic(fmt.Sprintf("Init config file failed: %v", err))
+	}
+
+	fmt.Println("Init config file success")
+}
---
+++ pkg/common/config.go
@@ -0,0 +1,13 @@
+package common
+
+type Configuration struct {
+	Mysql MysqlConfiguration `mapstructure:"mysql" json:"mysql"`
+}
+
+type MysqlConfiguration struct {
+	Username    string `mapstructure:"username" json:"username"`
+	Password    string `mapstructure:"password" json:"password"`
+	Database    string `mapstructure:"database" json:"database"`
+	Host        string `mapstructure:"host" json:"host"`
+	Port        int    `mapstructure:"port" json:"port"`
+}
